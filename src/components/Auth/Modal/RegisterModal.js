eccfee7b75a0860c8cf740d807c4a104f959cff0
KennyK-TW <b2khakashu@yahoo.com>
2019-12-10 23:14:09 -0800

    register format for input

--- src/components/Auth/Modal/RegisterModal.js
+++ src/components/Auth/Modal/RegisterModal.js
@@ -22,7 +22,7 @@ function RegisterModal(props) {
             <Modal.Body>
                 <div className="container mt-4">
                     <div className="row">
-                    <div className="col-m">
+                    <div className="col">
                         <form onSubmit={props.handleSubmit}>
                         <div className="form-group">
                             <label htmlFor="firstName">First Name</label>
